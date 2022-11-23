246f5cf8a0bb0caaa49eafc6b2ca0d78d87aeb51
Flor B. Córdoba <77332118+Flory3508@users.noreply.github.com>
2022-11-22 23:38:43 -0300

    practica typescript 1

---
+++ practica typescript/example01.ts
@@ -0,0 +1,10 @@
+let a: number; //*Inferencia explícita
+let b: string; //*Inferencia explícita
+let c=101;     //*Inferencia implícita
+
+//c="one"; --> esto me da error
+
+let recursos:['memoria','disco','procesador',11]; // es una mala práctica
+let recursos1:string[]=['memoria','disco','procesador'] //es una buena práctica , si coloco o agrego un numero me marca error
+
+
---
+++ practica typescript/output/app.js
@@ -0,0 +1,9 @@
+"use strict";
+let message = 'Hello world';
+console.log(message);
+let a; //*Inferencia explícita
+let b; //*Inferencia explícita
+let c = 101; //*Inferencia implícita
+//c="one"; --> esto me da error
+let recursos; // es una mala práctica
+let recursos1 = ['memoria', 'disco', 'procesador']; //es una buena práctica , si coloco o agrego un numero me marca error
